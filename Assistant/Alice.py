f8d73d3d9505c9d062696c540c8468cc504e2571
Brodevil <brodevil89@gmail.com>
2021-04-28 14:08:10 +0530

    Adding the temperature API and also location API

--- Assistant/Alice.py
+++ Assistant/Alice.py
@@ -1,9 +1,13 @@
 import pyttsx3
 import datetime
+import requests
+import json
+import pprint
 
 
 engine = pyttsx3.init('sapi5')
 engine.setProperty('voice', engine.getProperty('voices')[0].id)
+engine. setProperty("rate", 150)
 
 intoduction = "Now me to introduce myself, I m Alice. A virtual desktop assitant and I'm here to assit you with a veriety of tasks \
         as best as I can. 24 Hours a day seven days a week, Importing all preferences from home, interface system are now \
@@ -15,7 +19,7 @@ def speak(audio):
 
 
 def intro():
-    hour = int(datetime.datetime.now().hour())
+    hour = int(datetime.datetime.now().hour)
     if hour == 0 and hour < 12:
         speak("Good Morning Sir!")
     
@@ -25,12 +29,23 @@ def intro():
     else:
         speak("Good Evening Sir!")
 
-    speak("Now me to introduce myself, I m Alice. A virtual desktop assitant and I'm here to assit you with a veriety of tasks \
-        as best as I can. 24 Hours a day seven days a week, Importing all preferences from home, interface system are now \
-        fully operational, Sir!")
+    speak("Now me to introduce myself, I m Alice. A virtual desktop assistant and I'm here to assit you with a veriety of tasks \
+        as best as I can. 24 Hours a day, seven days a week, Importing all preferences from home Interface, System are now \
+        fully operational!")
+
+
+
+def temperature():
+    try:
+        response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=thane&appid=c04f06f6ac189dc5401ecf14a257adc7")
+        respons = json.load(response.text)
+        pprint.pprint(response)
+    except ConnectionError:
+        return 
+
 
 
 if __name__ == "__main__":
-    speak("Abhinav is the good boy!")
-    
+    # intro()
+    temperature()
 
