3d8cb3ddada66ad6fc004d86df9eb1e163cba3aa
Brodevil <brodevil89@gmail.com>
2021-05-05 14:02:34 +0530

    functions for the reminding the user with notification

---
+++ Assistant/exts/reminder.py
@@ -0,0 +1,39 @@
+import datetime
+from plyer import notification
+from Assistant.__init__ import speakRichard 
+
+
+def notifier(reason, string, ico):
+    notification.notify(
+                    title = string,
+                    message = reason,
+                    app_icon = ico,
+                    timeout = 12
+                )
+            
+
+def reminder(magnitude, unit, pourpose=None):
+    if unit.lower() == "minutes":
+        remindTime = magnitude + int(datetime.datetime.now().minute)
+        while(True):
+            presentTime = int(datetime.datetime.now().minute)
+            if presentTime == remindTime:
+                speakRichard("Time Out Sir!")
+                notifier(
+                    reason=pourpose,
+                    string="Alice : Time Out Sir!",
+                    ico="timeout.svg"
+                )
+
+    elif unit.lower() == "hours":
+        remindTime = magnitude + int(datetime.datetime.now().hours)
+        while(True):
+            presentTime = int(datetime.datetime.now().hours)
+            if presentTime == remindTime:
+                notifier(
+
+                )
+
+
+if __name__ == "__main__":
+    pass
