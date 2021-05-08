795118b1a47eebfc501efb0209a5f71a12b1fb8f
Brodevil <brodevil89@gmail.com>
2021-05-08 20:19:39 +0530

    Added the location free api returns several local info

--- Assistant/exts/apis.py
+++ Assistant/exts/apis.py
@@ -1,13 +1,27 @@
 import requests
 import json
-
-def temperature(location):
-    pass
+import pprint
 
 
-def location():
-    pass
 
+def weather(location):
+    try:
+        response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=thane&appid=c04f06f6ac189dc5401ecf14a257adc7")
+        response = json.loads(response.text)
+        return int(response['main']['feels_like'] - 273.15)
+    except ConnectionError:
+        return
+    
+
+
+def localInfo():
+    url = "http://ip-api.com/json/"
+    response = requests.get(url)
+    response = json.loads(response.text)
+    del response['status'], response['countryCode'], response['region']
+    return [response['city'], response]
+    
+    
 
 
 def news(url="https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apikey=96926ea85ee242508e5527e0891be103", apikey="96926ea85ee242508e5527e0891be103"):
@@ -23,5 +37,7 @@ def news(url="https://newsapi.org/v2/top-headlines?sources=the-times-of-india&ap
     return json_data
 
 
+
+
 if __name__ == "__main__":
-    pass
+    localInfo()
