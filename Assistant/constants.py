781b403d14381eae0ecdd681beed003639c1dcb5
Brodevil <brodevil89@gmail.com>
2021-05-11 14:41:30 +0530

    ctrl+x the function of readExel and put it in a new file

--- Assistant/constants.py
+++ Assistant/constants.py
@@ -1,26 +1,3 @@
-import openpyxl
-import pprint
-
-
-def readExel(path):
-    """Read the Exel file using openpyxl and return the 2d list as 1st will the time and 2nd will the purpose """
-
-    # workbook object is created
-    wb_obj = openpyxl.load_workbook(path)
-    sheet_obj = wb_obj.active
-    max_col = sheet_obj.max_column
-    records = list()
-
-    # Loop will returning all columns name
-    for j in range(1, sheet_obj.max_row + 1):
-        for i in range(1, max_col + 1):
-            if i == 2 or i == 3 or i == 5:
-                content = sheet_obj.cell(row=j, column=i)
-                records.append(content.value)
-    records = {records[i]:[records[i+1], records[i+2]] for i in range(0, len(records)-1, 3)}
-    return records
-         
-
 class Contacts:
     emails = {
         "shivam":"shivamgopale31@gmail.com",
@@ -99,4 +76,4 @@ POSITIVE_REPLIES = [
 
 
 if __name__ == "__main__":
-    readExel(r"contactinfo.xlsx")
+    pass
