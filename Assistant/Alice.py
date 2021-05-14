6fda357a8bad25f7449aabd460c766dceb898b27
Brodevil <brodevil89@gmail.com>
2021-05-14 17:09:51 +0530

    Added good wished and into

--- Assistant/Alice.py
+++ Assistant/Alice.py
@@ -12,6 +12,18 @@ from constants import Contacts, ERROR_REPLIES, NEGATIVE_REPLIES, POSITIVE_REPLIE
 load_dotenv()
 
 
+def goodWish():
+    presentHour = int(datetime.datetime.now().hour)
+    if presentHour == 0 or presentHour < 12:
+        return "Good Morning"
+
+    elif presentHour == 12 or presentHour < 18:
+        return "Good Afternoon"
+
+    else:
+        return "Good Evening"
+
+
 class Alice:
     """ 
     Alice Assitant:
@@ -21,23 +33,20 @@ class Alice:
 
     def __init__(self):
         super().__init__()
-        self.name = environ.get("UserName", "Abhinav")
-
-        gender = environ.get("GENDER", 'male')
+        self.name = environ.get("UserName", "Abhinav")      # this is the user name of the person who suppose to use this program : Data From (.env)
+        self.gender = environ.get("GENDER", 'male')          # Gender of the user, Data From (.env)
         if gender == "male" or gender == "boy":
             self.gender = "Sir"
         elif gender == "female" or gender == "girl":
             self.gender == "Mam"
 
-        
-        self.city = environ.get("location", localInfo())
-        
+        self.city = environ.get("location", localInfo())    # Data From (.env)
         self.voice = Client.voice
         self.voiceSpeed = Client.voiceRate
         
 
 
-    def severalVoices(self, voicesId):
+    def severalVoices(self, voicesId=Client.voices):
         """ This is the function to show the user how many voices are available in his/her system 
         So that the user will able to choose his own liked voice """
 
@@ -46,15 +55,17 @@ class Alice:
             engine.setProperty("voice", voice)
             engine.setProperty("rate", 170)
             if index == 0:
-                engine.say(f"Hey there! My voice name is {voice.split('Tokens')[1].replace('_', ' ')}")
-                engine.say(f"Hey there! I am {index+1}st voice of your system {self.gender}!")
+                engine.say(f"Hey there! I am {index+1}st voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
+                print(f"{Client.name} : Hey there! I am {index+1}st voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file\n")
             elif index == 1:
-                engine.say(f"Hey there! I am {index+1}nd voice of your system {self.gender}!")
+                engine.say(f"Hey there! I am {index+1}nd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
+                print(f"{Client.name} : Hey there! I am {index+1}nd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file\n")
             elif index == 2:
-                engine.say(f"Hey there! I m {index+1}rd voice of your system {self.gender}!")
+                engine.say(f"Hey there! I m {index+1}rd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
+                print(f"{Client.name} : Hey there! I am {index+1}rd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file\n")
             else:
-                engine.say(f"Hey there! I am {index+1}th voice of your system {self.gender}!")
-
+                engine.say(f"Hey there! I am {index+1}th voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
+                print(f"{Client.name} : Hey there! I am {index+1}th voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file\n")
             engine.runAndWait()
 
 
@@ -62,30 +73,21 @@ class Alice:
         """ Speak function as per the selected voice """
         
         engine = pyttsx3.init('sapi5')
-        try:
-
-
+        engine.setProperty("voice", Client.voices[self.voice])
         engine.setProperty("rate", self.voiceSpeed)
+        print(f"{Client.name} :  {audio}\n")
         engine.say(audio)
         engine.runAndWait()
     
 
     def intro(self):
-        hour = int(datetime.datetime.now().hour)
-        if hour == 0 or hour < 12:
-            speak(f"Good Morning {self.name} {self.gender}")
-
-        elif hour == 12 or hour < 18:
-            speak(f"Good Afternon {self.name} {self.gender}")
-
-        else:
-            speak(f"Good Evening {self.name} {self.gender}")
-
         speak(Client.intro)
+        
 
 
 
 
 if __name__ == "__main__":
-    print(Client.voices)
-    Alice.severalVoices(Client.voices)
+    test0 = Alice()
+    print(test0.city)
+    test0.severalVoices()
