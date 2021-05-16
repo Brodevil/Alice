e04134bed3b6336d2494ce27b267f8f2cf33eaae
Brodevil <brodevil89@gmail.com>
2021-05-16 20:33:11 +0530

    Used pep 8 style in the file

--- Assistant/Alice.py
+++ Assistant/Alice.py
@@ -9,19 +9,11 @@ from exts.networks import localInfo                                             
 from constants import Contacts, ERROR_REPLIES, NEGATIVE_REPLIES, POSITIVE_REPLIES, Client             # noqa
 
 
+__all__ = ("Alice", "alice")
+
+
 load_dotenv()
 
-
-def goodWish():
-    presentHour = int(datetime.datetime.now().hour)
-    if presentHour == 0 or presentHour < 12:
-        return "Good Morning"
-
-    elif presentHour == 12 or presentHour < 18:                                                       # noqa
-        return "Good Afternoon"
-
-    else:
-        return "Good Evening"
 
 
 class Alice:
@@ -31,14 +23,13 @@ class Alice:
     This class contain the Function of the Alice program and also with the Assitant Data
     """
 
+
     def __init__(self):
         super().__init__()
+        gender=str(environ.get("GENDER", 'male'))
         self.name = environ.get("UserName", "Abhinav")      # this is the user name of the person who suppose to use this program : Data From (.env)
-        gender = str(environ.get("GENDER", 'male')).lower()         # Gender of the user, Data From (.env)
-        if gender == "male" or gender == "boy":
-            self.gender = "Sir"
-        elif gender == "female" or gender == "girl":
-            self.gender == "Mam"
+        self.gender = ("Sir" if gender == "male" else "Mam")
+
 
         self.city = environ.get("location", localInfo())    # Data From (.env)
         self.voice = Client.voice
@@ -55,20 +46,25 @@ class Alice:
             engine.setProperty("voice", voice)
             engine.setProperty("rate", 170)
             if index == 0:
-                engine.say(f"Hey there! I am {index+1}"+"st"+ " voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
                 print(f"{Client.name} : Hey there! I am {index+1}st voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file\n")
+                engine.say(f"Hey there! I am {index+1}"+"st"+ " voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
+                
+           
             elif index == 1:
-                engine.say(f"Hey there! I am {index+1}nd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
                 print(f"{Client.name} : Hey there! I am {index+1}nd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file\n")
+                engine.say(f"Hey there! I am {index+1}nd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
+
             elif index == 2:
-                engine.say(f"Hey there! I m {index+1}rd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
                 print(f"{Client.name} : Hey there! I am {index+1}rd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file\n")
+                engine.say(f"Hey there! I m {index+1}rd voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
+            
             else:
+                print(f"{Client.name} : Hey there! I am {index+1}th voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file\n")
                 engine.say(f"Hey there! I am {index+1}th voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file")
-            print(f"{Client.name} : Hey there! I am {index+1}th voice of your system {self.gender}! You can select voice as a default by putting my VoiceNumber={index+1} in .env file\n")
+            
         engine.runAndWait()
 
-
+    
 
     def speak(self, audio):
         """ Speak function as per the selected voice """
@@ -81,6 +77,20 @@ class Alice:
         engine.runAndWait()
     
 
+    @property
+    @staticmethod
+    def goodWish():
+        presentHour = int(datetime.datetime.now().hour)
+        if presentHour == 0 or presentHour < 12:
+            return "Good Morning"
+
+        elif presentHour == 12 or presentHour < 18:                                                       # noqa
+            return "Good Afternoon"
+
+        else:
+            return "Good Evening"
+
+
     def intro(self):
         self.speak(Client.intro)
         self.speak(f"Storage : {Client.storage['Total']} GB, Memory Used : {Client.memory_status}%,  CPU Used : {Client.cpu_status}%")
@@ -89,16 +99,16 @@ class Alice:
             self.speak(Client.weatherInfo)   # Tring to say the weather report ond the client local area'
         except Exception:
             pass
-        self.speak(f"{goodWish()} {self.name} {self.gender}!, System is now fully Operational. How Can I help you {self.gender}")
+        self.speak(f"{self.goodWish} {self.name} {self.gender}!, System is now fully Operational. How Can I help you {self.gender}")
         if Client.internet == False:
             self.speak(f"{self.gender}! Internet is not connected. I going to work with Internet, Please get connect with internet.")
             exit()
         
-        
+
+alice = Alice()     # Object for the Alice class  
 
 
 if __name__ == "__main__":
     test0 = Alice()
-    test0.speak("Hey there!")
     test0.intro()
     # print(environ.get("GENDER"))
