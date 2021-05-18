d4c1ed8308d9e22d282453c0407032eea96d595a
Brodevil <brodevil89@gmail.com>
2021-05-18 19:56:48 +0530

    Initial commit

--- Assistant/constants.py
+++ Assistant/constants.py
@@ -73,6 +73,7 @@ class Client:
     # Few user Info :
     musicDirectory = environ.get("MUSIC", r"C:\Users\ADMIN\Music")              # Music directory should be without space
     favorateMusic = environ.get("FavMusic", None)
+    userGithub = environ.get("GITHUB", "")
 
 
     if battery != None:
