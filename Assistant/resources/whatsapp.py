1954d43b1925da3e57c4343d40b9868a0ccd2fea
Brodevil <brodevil89@gmail.com>
2021-05-20 13:54:48 +0530

    Created the simple templete

---
+++ Assistant/resources/whatsapp.py
@@ -0,0 +1,13 @@
+import pywhatkit
+import time
+from pprint import pprint 
+import requests
+import asyncio
+
+
+def sendWhatsappMessage(number, message):
+    pass
+
+
+def readWhatsappMessage():
+    pass
