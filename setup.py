6dc07af6b62863328713b598ecb35db8d11cb2ed
Brodevil <brodevil89@gmail.com>
2021-05-20 13:43:53 +0530

    Added the main part of the program in that file that is info and taking comamnd with processing

--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
+import Assistant
+from Assistant import alice
+
+
+
 if __name__ == "__main__":
-    import Assistant
+    # Running part of the Alice Program
+    alice.intro()       # Introdunction
+
+
+    while True:     # The program will be going to run on INfinate loop
+        queary = alice.takeCommand().lower()
+        if queary != "none":
+            Assistant.logic(queary)     # Logic for executin task based on query
