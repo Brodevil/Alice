ee9399d840baf562800f19c88903cb6a557d6eb3
Brodevil <brodevil89@gmail.com>
2021-05-20 22:31:15 +0530

    Aded __all__ and __authors__

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from Assistant import alice
 
 
 
+__all__ = ("intro", "queary", "Assistant.logic")
+__authors__ = ("Abhinav", "Brodevil")       # Both are the same person
+
+
+
 if __name__ == "__main__":
     # Running part of the Alice Program
     alice.intro()       # Introdunction
-
 
     while True:     # The program will be going to run on INfinate loop
         queary = alice.takeCommand().lower()
