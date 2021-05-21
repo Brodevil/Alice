9c97032d9d4a999e62eb102ab657d98785481849
Brodevil <brodevil89@gmail.com>
2021-05-21 14:10:22 +0530

    Initial commit

--- Assistant/exts/audioBook.py
+++ Assistant/exts/audioBook.py
@@ -1,2 +1,28 @@
 import pyttsx3
-import pypfd                                                                    # noqa
+import PyPDF2                                                                               # noqa
+import os
+# from constants import Client
+
+
+def audioBook(pdfPath, fromPageNo=0):
+    """ Funtion to read the pdf and save the audio in a mp3 file at the same directory where the pdf locatied """
+    full_Text = str()
+    with open(pdfPath, "rb") as book:
+        reader = PyPDF2.PdfFileReader(book)
+        audio_reader = pyttsx3.init('sapi5')
+        audio_reader.setProperty("rate", 170)
+        # audio_reader.setProperty("voice", Client.voices[Client.voice-1])
+        
+        for page in range(reader.numPages-fromPageNo):
+            next_page = reader.getPage(page+fromPageNo)
+            content = next_page.extractText()
+            full_Text+=content
+            
+
+        audio_reader.save_to_file(content, os.path.join(pdfPath))
+        audio_reader.runAndWait()
+
+    
+
+if __name__ == "__main__":
+    audioBook(r"Assistant\media\Machine Learning.pdf", 23)
