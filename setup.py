d730a2af2b67f85d799f39d4c3accbe24bdbe1b3
Brodevil <brodevil89@gmail.com>
2021-05-22 15:32:47 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ __authors__ = ("Abhinav", "Brodevil")       # Both are the same personm lol
 
 if __name__ == "__main__":
     # Running part of the Alice Program
-    alice.intro()       # Introdunction
+    # alice.intro()       # Introdunction
 
-    while True:     # The program will be going to run on INfinate loop
+    while True:     # The program will be going to run on Infinate loop
         queary = alice.takeCommand().lower()
         if queary != "none":
             Assistant.logic(queary)     # Logic for executin task based on query
