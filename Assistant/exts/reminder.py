2d2dbb6dadd8fabd4af6a184db7103c8b3c80836
Brodevil <brodevil89@gmail.com>
2021-05-28 14:37:04 +0530

    Intial commit

--- Assistant/exts/reminder.py
+++ Assistant/exts/reminder.py
@@ -1,22 +1,26 @@
 from plyer import notification
-import pyttsx3
 import time
 from Assistant.Alice import alice
 
 
 
 def notifier(reason, string, ico):
-    notification.notify(
-                    title = string,
-                    message = reason,
-                    app_icon = ico,
-                    timeout = 12
-                )
-            
+    try:
+        notification.notify(
+            title=string,
+            message=reason,
+            app_icon=ico,
+            timeout=12
+        )
+    except Exception:
+        pass
+
+
+
 
 def reminder(magnitude, unit, pourpose):
-    if unit.lower() == "myounute" or unit.lower() == "myounutes":     # this is actually the computer sense word to minutes as a myournutes     #noqa
-        remindTime = int(magnitude*60 + time.time())
+    if unit.lower() == "myounute" or unit.lower() == "myounutes":  # this is actually the computer sense word to minutes as a myounutes     #noqa
+        remindTime = int(magnitude * 60 + time.time())
         while True:
             presentTime = int(time.time())
             if presentTime == remindTime:
@@ -29,8 +33,8 @@ def reminder(magnitude, unit, pourpose):
                 break
 
     elif unit.lower() == "hours" or unit.lower() == "hour":
-        remindTime = int(magnitude*60*60 + time.time())
-        while(True):
+        remindTime = int(magnitude * 60 * 60 + time.time())
+        while True:
             presentTime = int(time.time())
             if presentTime == remindTime:
                 alice.speak("Time Out Sir!")
