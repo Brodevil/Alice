e02aa3c022e1f8b1fb433f54b61d313327fe8476
Brodevil <brodevil89@gmail.com>
2021-05-30 14:31:42 +0530

    Added function to read the taily task exel file and return the time and work as a dict

--- Assistant/exts/workWithFiles.py
+++ Assistant/exts/workWithFiles.py
@@ -2,37 +2,50 @@ import openpyxl
 import os
 
 
+__all__ = ["contactInfo", "deleteUnwantedFiles", "openApplication", "DailyWorksExel"]
+
 
 
 def contactInfo(path):
     """Read the Exel file using openpyxl and return the dictionary containing email id and phone number """
 
-    # workbook object is created
     wb_obj = openpyxl.load_workbook(path)
     sheet_obj = wb_obj.active
-    max_col = sheet_obj.max_column
     records = list()
 
     # Loop will returning all columns name
     for j in range(1, sheet_obj.max_row + 1):
-        for i in range(1, max_col + 1):
-            if i == 2 or i == 3 or i == 5:
-                content = sheet_obj.cell(row=j, column=i)
-                records.append(content.value)
-    records = {records[i]:[records[i+1], records[i+2]] for i in range(0, len(records)-1, 3)}
+        for i in range(1, sheet_obj.max_column + 1):
+            content = sheet_obj.cell(row=j, column=i)
+            records.append(content.value)
+    records = {records[i]: [records[i+1], records[i+2]] for i in range(0, len(records)-1, 3)}
     return records
+
+
+def DailyWorksExel(path):
+    wb_obj = openpyxl.load_workbook(path)
+    sheet_obj = wb_obj.active
+    task = dict()
+
+    for j in range(3, sheet_obj.max_row + 1):
+        workTime = sheet_obj.cell(row=j, column=1)
+        work = sheet_obj.cell(row=j, column=2)
+        task.update(workTime, work)
+
 
 
 
 def deleteUnwantedFiles():
+    """
+    The following directory contain the temporary files and just unwanted files
+    """
     unwantedFiles = [r"C:\Windows\Temp", r"C:\Users\ADMIN\AppData\Local\Temp", r"C:\Windows\Prefetch"]
     for f in unwantedFiles:
         for file in os.listdir(f):
             try:
-                os.remove(os.path.join(f , file))  
+                os.remove(os.path.join(f, file))
             except PermissionError:
                 pass
-
 
 
 def openApplication(ApplicationName: str, installedApplicationPath: str):
@@ -47,6 +60,7 @@ def openApplication(ApplicationName: str, installedApplicationPath: str):
                 os.startfile(os.path.join(installedApplicationPath, app))
                 return app.split(".")[0]
     return None
+
 
 
 
