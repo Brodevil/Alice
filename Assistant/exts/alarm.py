0896a7e8d79d65cc9bed651208b3ecb4bf2b0b7a
Brodevil <brodevil89@gmail.com>
2021-06-06 14:52:05 +0530

    Tooked the full path of notification icon, reduce code by not repeting my code, running while True just one time

--- Assistant/exts/alarm.py
+++ Assistant/exts/alarm.py
@@ -2,7 +2,7 @@ from plyer import notification
 import time
 import winsound
 import pyttsx3
-from os import environ
+from os import environ, getcwd
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,7 +12,7 @@ __all__ = ["notifier", "reminderAlarm"]
 
 def speak(str):
     engine = pyttsx3.init("sapi5")
-    engine.setProperty("voice", engine.getProperty("voices")[int(environ.get("VoiceNumber", 1))-1].id)
+    engine.setProperty("voice", engine.getProperty("voices")[int(environ.get("VoiceNumber", 1)) - 1].id)
     engine.say(str)
     engine.runAndWait()
 
@@ -32,31 +32,22 @@ def notifier(reason, string, ico):
 def reminderAlarm(magnitude, unit, pourpose):
     if unit.lower() == "myounute" or unit.lower() == "myounutes" or unit.lower() == "minute" or unit.lower() == "minutes":  # this is actually the computer sense word to minutes as a myounutes     #noqa
         remindTime = int(magnitude * 60 + time.time())
-        while True:
-            presentTime = int(time.time())
-            if presentTime == remindTime:
-                notifier(
-                    reason=pourpose,
-                    string="Alice : Time Out Sir!",
-                    ico=r"Assistant\media\time-out.ico"
-                )
-                winsound.Beep(frequency=2500, duration=4000)
-                speak(
-                    f"Time Up, You had told me to remind your after {magnitude} minutes, Now its time to remind you, Wake up.")
-                break
+        is_hour = False
 
     elif unit.lower() == "hours" or unit.lower() == "hour":
         remindTime = int(magnitude * 60 * 60 + time.time())
-        while True:
-            presentTime = int(time.time())
-            if presentTime == remindTime:
-                notifier(
-                    reason=pourpose,
-                    string="Alice : Time Out Sir!",
-                    ico=r"Assistant\media\time-out.ico"
-                )
-                winsound.Beep(frequency=2500, duration=4000)
-                speak(
-                    f"Time Up, You had told me to remind your after {magnitude} hours, Now its time to remind you, Wake up.")
-                break
+        is_hour = True
 
+    while True:
+        presentTime = int(time.time())
+        if presentTime == remindTime:  # noqa
+            notifier(
+                reason=pourpose,
+                string="Alice : Time Out Sir!",
+                ico=getcwd().replace("\\Alice\\Assistant\\exts", "\\Alice\\Assistant\\media\\time-out.ico")
+            )
+            winsound.Beep(frequency=2500, duration=4000)
+            speak(
+                f"Time Up, You had told me to remind your after {magnitude}" " hours " if is_hour else " minutes" +                         # noqa
+                                                                                                       "Now its time to remind you, Wake up.")
+            break
