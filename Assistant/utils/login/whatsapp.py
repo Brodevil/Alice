31230bd2fa13e31cb6c0d1c4e471fb213a577f15
Brodevil <brodevil89@gmail.com>
2021-06-15 14:41:28 +0530

    Testing process

--- Assistant/utils/login/whatsapp.py
+++ Assistant/utils/login/whatsapp.py
@@ -5,8 +5,12 @@ __all__ = ("sendWhatsappMessage", "readWhatsappMessage")
 
 
 def sendWhatsappMessage(number, message):
-    pass
+    pywhatkit.sendwhatmsg_instantly("+918975057675", "Hey", wait_time=5)
 
 
 def readWhatsappMessage():
     pass
+
+
+if __name__ == '__main__':
+    sendWhatsappMessage(None, None)
