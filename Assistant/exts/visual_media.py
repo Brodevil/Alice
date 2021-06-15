0801b2134ec90cf1d70a28eed223b14e41844a1e
Brodevil <brodevil89@gmail.com>
2021-06-15 13:53:35 +0530

    template ready

--- Assistant/exts/visual_media.py
+++ Assistant/exts/visual_media.py
@@ -1,41 +1,30 @@
-from cv2 import *
-import sounddevice as sd
-from scipy.io.wavfile import write
-import datetime
 
+
+
+__all__ = ["VisualMedia"]
 
 
 class VisualMedia:
     def __init__(self):
-        self.cam = VideoCapture(0)      # initialize the camera
-
+        pass
 
     def photos(self):
-        # initialize the camera
-        s, img = self.cam.read()
-        if s:    # frame captured without any errors
-            namedWindow("Alice", 500)
-            imshow("Alice", img)
-            waitKey(0)
-            imwrite(f"Alice({datetime.time().strftime('%b-%d-%Y')}).jpg", img)
-            destroyAllWindows()
-
+        pass
 
     def video_recorder(self):
         pass
 
-
     def screen_recorder(self):
         pass
-
 
     def CCTV_mood(self):
         pass
 
-
     def voice_recorder(self):
         pass
 
+    def screen_shorts(self):
+        pass
 
 
 if __name__ == '__main__':
