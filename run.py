92b463292d6d2705a2ba3d1370638be2a8ed8edc
Brodevil <brodevil89@gmail.com>
2021-06-18 16:04:53 +0530

    Path changed

--- run.py
+++ run.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
         if 'sleep' in queary or 'take a break' in queary:
             alice.speak(f"{Client.GENDER}! I am going to sleep while you don't wake up me")
-            startfile(path.join(Client.ALICE_PATH, "Media//Ribbons.scr"))
+            startfile(path.join(Client.ALICE_PATH, "Assistant//resources//images//Ribbons.scr"))
 
             while 'wake up' not in queary and 'back to work' not in queary:
                 queary = alice.takeCommand()
