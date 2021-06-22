5b8e7416ce82c43c3b62727177d7cece325bff8f
Brodevil <brodevil89@gmail.com>
2021-06-22 20:38:49 +0530

    Added a alarm fucntion to just reminding

--- Assistant/exts/alarm.py
+++ Assistant/exts/alarm.py
@@ -1,27 +1,31 @@
-from plyer import notification
 import time
-import winsound
+import winsound  # noqa
 import pyttsx3
-from os import environ, getcwd
-from dotenv import load_dotenv
+import datetime
 
+from os import environ, getcwd, path
+from dotenv import load_dotenv
+from plyer import notification
+
+from Assistant.constants import Client
 
 load_dotenv()
 
 __all__ = ["speak", "notifier", "reminderAlarm"]
 
 
-def speak(string):
+def speak(string: str) -> None:
     """
     speak function for reminding the user from voice also
     """
     engine = pyttsx3.init("sapi5")
     engine.setProperty("voice", engine.getProperty("voices")[int(environ.get("VoiceNumber", 1)) - 1].id)
+    print(f"{Client.ASSISTANT_NAME} : {string}", "\n")
     engine.say(string)
     engine.runAndWait()
 
 
-def notifier(reason, string, ico):
+def notifier(reason: str, string: str, ico: str) -> None:
     """
     take the reason, long string and icon file as a argument
     and make a notification in windows 10 for reminding the user in any task or work
@@ -37,7 +41,7 @@ def notifier(reason, string, ico):
         pass
 
 
-def reminderAlarm(magnitude, unit, pourpose):
+def reminderAlarm(magnitude: int, unit: str, pourpose: str) -> None:
     """
     The function is just a reminder not a alarm
     this will remind after specific time, by beeping and notifying
@@ -51,23 +55,39 @@ def reminderAlarm(magnitude, unit, pourpose):
         remindTime = int(magnitude * 60 * 60 + time.time())
         is_hour = True
     else:
-        reminderTime = None
+        return
 
     while True:
         presentTime = int(time.time())
         if presentTime == remindTime:  # noqa
             notifier(
-                reason=pourpose,
+                reason=f"Here's your reminder : {pourpose}",
                 string="Alice : Time Out Sir!",
-                ico=getcwd().replace("\\Alice", "\\Alice\\Assistant\\resources\\Images\\time-out.ico")
+                ico=path.join(Client.ALICE_PATH, "Assistant\\resources\\Images\\time-out.ico")
             )
             winsound.Beep(frequency=2500, duration=4000)
             speak(
-                f"Time Up, You had told me to remind your after {magnitude}" " hours " if is_hour else " minutes" +  # noqa
-                                                                                                       "Now its time to remind you, Wake up.")
+                f"Time Up, You had told me to remind you after {magnitude}" " hours " if is_hour else " minutes" +  # noqa
+                                                                                                      "Now its time to remind you, Wake up.")
+
             break
 
 
+def alarm(time_unit: str):
+    """
+    Alarm function to remind at the specific time like at 19:05/02:00
+    """
 
+    while True:
+        current_time = datetime.datetime.now().strftime("%H:%M")
+        if current_time == time_unit:
+            notifier(
+                reason=f"Alarm of {time_unit}",
+                string="Time has arrived!",
+                ico=path.join(Client.ALICE_PATH, "Assistant\\resources\\Images\\time-out.ico")
+            )
+            winsound.Beep(frequency=2500, duration=4000)
+            speak(f"The Alarm of {time_unit} had Been Arrived!")
+            break
 
 
